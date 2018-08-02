0d4084c5b07106358916a72c025c0bb3148ddd02
Tayte Stokes <TayteStokes@Taytes-MacBook-Air.local>
2018-08-01 21:28:42 -0600

    Made some more adjustments to project three

--- src/Components/ProjectThree/ProjectThree.js
+++ src/Components/ProjectThree/ProjectThree.js
@@ -14,7 +14,7 @@ class ProjectThree extends Component {
         <div className="project-one-info">
             <h1>Flickr Clone</h1>
             <h2>React | Redux | NodeJs | ExpressJs</h2>
-            <p>This is a clone of the flickr website. It's still currently a work in progress, but I decided to clone this site to use their API.</p>
+            <p>This is a clone of the flickr website. I decided to clone this site to play with their public API. For the front-end, I am using React with Redux for state management. The back-end consists of a NodeJs environment using an ExpressJs framework.</p>
             <div className="project-one-links">
                 <a href="#"><h1>Live Site</h1></a>
                 <a href="https://github.com/Tstokes9328/flickr-clone"><h2>Github</h2></a>
@@ -29,7 +29,7 @@ class ProjectThree extends Component {
                     <div className="phone-camera"></div>
                     <div className="phone-speaker"></div>
                 </div>
-                {/* <img src={require('#')} alt="Flickr Clone"/> */}
+                <img src={require('../../assets/flickr.png')} alt="Flickr Clone"/>
             </div>
         </div>
         </Slide>
