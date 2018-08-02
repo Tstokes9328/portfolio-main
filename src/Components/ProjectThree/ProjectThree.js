c07bfa58c82be278f62c28b19d24675569c26630
Tayte Stokes <TayteStokes@Taytes-MacBook-Air.local>
2018-08-01 21:23:50 -0600

    added a link to flickr github repo

--- src/Components/ProjectThree/ProjectThree.js
+++ src/Components/ProjectThree/ProjectThree.js
@@ -17,7 +17,7 @@ class ProjectThree extends Component {
             <p>This is a clone of the flickr website. It's still currently a work in progress, but I decided to clone this site to use their API.</p>
             <div className="project-one-links">
                 <a href="#"><h1>Live Site</h1></a>
-                <a href="#"><h2>Github</h2></a>
+                <a href="https://github.com/Tstokes9328/flickr-clone"><h2>Github</h2></a>
             </div>
         </div>
         </Slide>
