2a5f0bd8aba8c9aed470e98325c822c56eabdb04
Tayte Stokes <TayteStokes@Taytes-MacBook-Air.local>
2018-08-04 17:06:51 -0600

    Reconstructed Footer / Ready for deployment

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,32 +9,7 @@ import './Footer.css';
 export default () => {
   return (
     <div className="footer-container">
-        <div className="footer-info-cont">
-        <div className="footer-name">
-            <h1>Name</h1>
-            <h2>Tayte Stokes</h2>
-        </div>
-
-        <div className="footer-phone">
-            <h1>Phone No.</h1>
-            <h2>(801) 664 9328</h2>
-        </div>
-
-        <div className="footer-email">
-            <h1>Email</h1>
-            <h2>taytestokes9328@gmail.com</h2>
-        </div>
-
-        <div className="footer-address">
-            <h1>Location</h1>
-            <h2>Lehi, UT</h2>
-        </div>
-        </div>
-
-        <div className="footer-links">
-            <a href="https://github.com/Tstokes9328"><FontAwesomeIcon icon={['fab', 'github']}/></a>
-            <a href="https://www.linkedin.com/in/tayte-stokes-1a545b165/"><FontAwesomeIcon icon={['fab', 'linkedin']}/></a>
-        </div>
+        <h1>Tayte Stokes 2018</h1>
     </div>
   )
 }
